d56d2e3c1c0252fbfad6c69e7e97b42b61a193fc
benjamin kunkel <benjamink1125@gmail.com>
2022-11-01 19:25:22 -0700

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+
+        public Car(int Year, int Make, string, Model, string EngineNoise, string HonkNoise, string IsDriavable)
+        {
+
+            Year = Year;
+            Make = Make;
+            Model = Model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriavable = isDriavable;
+        }
+
+
+
+
+
+        Public int Year { get , set; }
+
+            Public int Make {  get , set;  }
+            Public string Model {  get , set; }
+            Public string EngineNoise {  get , set; }
+            Public string HonkNoise {  get , set; }
+            Public bool IsDriavable {  get , sat; }
+
+            public void MakeEngineNoise (string engineNoise)
+        {
+            Console.WriteLine($"This car goes {EngineNoise}");
+
+            public void MakeHornNoise (string hornNoise)
+            {
+                Console.WriteLine($"The horn goes {hornNoise}");
+            }
+        }
+
+
+
+
+    }
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        var car1 = new Car();
+        car1. Year = 2016;
+            car1. Make = "Mercedes";
+            car1. Model = "C300"
+            car1. EngineNoise = "Good";
+            car1. HonkNoise = "Beep Beep";
+            car1. IsDriavable = true;
+
+            var car2 = new Car() { Year = 2017, Make = "Toyota", Model = "Corolla", EngineNoise = "quiet", HonkNoise = "Beep Beep", IsDriaveable = True };
+
+
+
+        var car3 = new Car(2020, "Honda", "Civic", "Vroom", "Loud Honk", true);
+    } 
+}
+
